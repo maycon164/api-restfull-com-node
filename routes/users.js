bbe657249a3af5bac33870b370227fa1a3b5fee4
maycon164 <mayconfelipe164@gmail.com>
2021-09-07 21:01:58 -0300

    route delete by id

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
 
     });
 
-    //buscaPeloNome
+    //buscaPeloNome ou pelo _id
     app.get('/users/:key', (req, res) => {
 
         //BUSCA pode ser pelo id do user ou pelo name;
@@ -46,7 +46,6 @@ module.exports = (app) => {
     });
 
     // inserindo registro
-
     app.post('/users', (req, res) => {
         
         db.insert(req.body, (error, doc) => {
@@ -60,6 +59,25 @@ module.exports = (app) => {
         })
         
     });
+
+    //Deletando um registro
+
+    app.delete('/users/:id', (req, res) => {
+        let id = req.params.id;
+
+        //Remove apenas com o id
+        db.remove({_id : id}, {multi : false}, (error, numRemoved) => {
+            
+            if(error){
+                
+                app.utils.error.send(error, req, res);
+
+            }else{
+                res.status(200).end(`<p> ${numRemoved} item foi removido <p>`);
+            }
+
+        });
+    })
 
     app.get('/users/admin', (req, res) => {
         res.statusCode = 200;
